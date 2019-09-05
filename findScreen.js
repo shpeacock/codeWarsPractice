b50fbdf00bed1de2b310929769b48e4d4455ba95
Spencer Peacock <spence.peacock@ezlynx.com>
2019-09-05 10:25:36 -0500

    looking at smarter ways

--- findScreen.js
+++ findScreen.js
@@ -4,6 +4,14 @@ function findScreenHeight(width, ratio) {
     let ratioSplit = ratio.split(":");
     let height = (parseInt(ratioSplit[1])/parseInt(ratioSplit[0]))*width;
     return str.concat(width, 'x', height);
+
+    //smarter ways
+    // var arr=ratio.split(':');
+    // return width+'x'+width/arr[0]*arr[1];
+
+    //clever clever
+    var [numerator, denominator] = ratio.split(':')  
+    return width + 'x' + width * denominator / numerator
 }
 console.log(findScreenHeight(1024, "4:3"));
 console.log(findScreenHeight(1280,"16:9"));
